bd7f3985c4745614ba572133907e95e23c6fe830
Bittl <bittl100@gmail.com>
2017-09-05 18:38:18 +0300

    Начало

---
+++ src/BeatBox.java
@@ -0,0 +1,107 @@
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class BeatBox {
+
+    JPanel mainPanel;
+    ArrayList<JCheckBox> checkBoxList;
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+    JFrame theFrame;
+
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap",
+                                "Hight Tom", "Hight Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap",
+                                "Low-mid Tom", " High Agogo", "Open Hi Conga"};
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+
+    public static void main(String[] args) {
+        new BeatBox2().buildGUI;
+    }
+
+    public void buildGUI(){
+        theFrame = new JFrame("Cyber BeatBox");
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout();
+        JPanel bacground = new JPanel(layout);
+        bacground.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        checkBoxList = new ArrayList<>();
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+
+        JButton start = new JButton("Start");
+        start.addActionListener(new MyStartListener());
+        buttonBox.add(start);
+
+        JButton stop = new JButton("Stop");
+        stop.addActionListener(new MyStopListener());
+        buttonBox.add(stop);
+
+        JButton upTempo = new JButton("Tempo Up");
+        stop.addActionListener(new MyUpTempoListener());
+        buttonBox.add(upTempo);
+
+        JButton downTempo = new JButton("Tempo Down");
+        stop.addActionListener(new MyDownTempoListener());
+        buttonBox.add(downTempo);
+
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+        for (int i = 0; i < 16; i++) {
+            nameBox.add(new Label(instrumentNames[i]));
+        }
+
+        bacground.add(BorderLayout.EAST, buttonBox);
+        bacground.add(BorderLayout.WEST, nameBox);
+
+        theFrame.getContentPane().add(bacground);
+
+        GridLayout grid = new GridLayout(16, 16);
+        grid.setVgap(1);
+        grid.setHgap(2);
+        mainPanel = new JPanel(grid);
+        bacground.add(BorderLayout.CENTER, mainPanel);
+
+        for (int i = 0; i < 256; i++) {
+            JCheckBox c = new JCheckBox();
+            c.setSelected(false);
+            checkBoxList.add(c);
+            mainPanel.add(c);
+        }
+
+        setUpMidi();
+
+        theFrame.setBounds(50, 50, 300, 300);
+        theFrame.pack();
+        theFrame.setVisible(true);
+    }
+
+    public void setUpMidi(){
+        try{
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void buildTrackAndStart(){
+        int[] trackList = null;
+
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+
+        for (int i = 0; i < 16; i++) {
+            trackList = new int[16];
+
+            int key = instruments[]
+        }
+    }
+}
